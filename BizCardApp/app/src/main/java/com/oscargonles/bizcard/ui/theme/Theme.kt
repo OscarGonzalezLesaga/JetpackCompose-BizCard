621cf8cb0416bac1971bf3a019607d20348fc696
Oscar Gonzalez <oscar.gonzalez@schustermann-borenstein.de>
2021-12-19 18:28:17 +0100

    use last version kotlin compiler

--- BizCardApp/app/src/main/java/com/oscargonles/bizcard/ui/theme/Theme.kt
+++ BizCardApp/app/src/main/java/com/oscargonles/bizcard/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun BizCardTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun BizCardTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
